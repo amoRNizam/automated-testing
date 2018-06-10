0c9c367d87ce4713f4bdc0d703efe0c417a070d5
amoRNizam <mazin.roma1992@yandex.ru>
2018-06-10 11:56:01 +0300

    Сохранение и загрузка куков. Второй пример. Он лучше

--- cookie.py
+++ cookie.py
@@ -1,4 +1,5 @@
 import pickle
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -11,25 +12,29 @@ elem = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div[1]/
 elem.click()
 # ищу поля логин-пароль
 elem = driver.find_element_by_name("login")
-elem.send_keys("asme25235n2004@yandex.ru")
+elem.send_keys("")
 elem = driver.find_element_by_name("passwd")
-elem.send_keys("1423418525510984qw")
+elem.send_keys("89525510984qw")
 # кнопка авторизации
 elem = driver.find_element_by_xpath("//*[@id='root']/div/div[2]/div[1]/div/div/div/div/div/form/div[4]/button[1]/span/span")
 elem.click()
-# cookies dump
-pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
+time.sleep(3)
+# Сохраним куки
+pickle.dump( driver.get_cookies() , open("cookies.pkl","wb"))
+driver.quit()
 # закрываю браузер  - куки в браузере не сохраняются
-driver.close()
+# driver.close()
 # открываю браузер заново
 #driver = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
 driver = webdriver.Chrome()
+driver.get("https://mail.yandex.ru/")
 # загружаю куки из дампа
 cookies = pickle.load(open("cookies.pkl", "rb"))
-print(cookies)  # смотрю что я загрузил из дампа
-driver.delete_all_cookies()  # выпилю старые прежде чем загрузить новые
+#---print(cookies)  # смотрю что я загрузил из дампа
+#---driver.delete_all_cookies()  # выпилю старые прежде чем загрузить новые
+# cookies = pickle.load(open("cookies.pkl", "rb"))
 for cookie in cookies:
     driver.add_cookie(cookie)
 
 # открываю страничку для которой делал дамп куки
-driver.get("https://yandex.ru/")
+#---driver.get("https://yandex.ru/")
