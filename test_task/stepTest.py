694cd51807ed02c68360bd087dfc60d9627958f5
amoRNizam <mazin.roma1992@yandex.ru>
2018-06-22 15:57:21 +0300

    Доработано

--- test_task/stepTest.py
+++ test_task/stepTest.py
@@ -32,9 +32,7 @@ class TestBuySitilink(unittest.TestCase):
             computers = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/noindex/ul/li[2]/a")
         except Exception:
             print(sys.exc_info()[1])
-#            raise
-#            e = sys.exc_info()[1]
-#            print(e.args[0])
+            self.fail("file not found")
 
         actions = ActionChains(self.driver)
         actions.move_to_element(computers).perform()
@@ -42,19 +40,20 @@ class TestBuySitilink(unittest.TestCase):
         #Кликнуть на раздел с компьютерами:
         try:
             notebook = wait.until(EC.element_to_be_clickable((By.XPATH,
-                                                                  "/html/body/div[1]/div/div[2]/noindex/ul/li[2]/div/div/a[1]")))
+                                                              "/html/body/div[1]/div/div[2]/noindex/ul/li[2]/div/div/a[1]")))
             actions.move_to_element(notebook).click().perform()
         except Exception:
             print(sys.exc_info()[1])
+            self.fail("There are no element")
 
         #Установить параметры фильтра:
         #Цена*******
         try:
             priceMax = wait.until(EC.element_to_be_clickable((By.NAME, "Цена до")))
-#            actions.move_to_element(priceMax).send_keys(u"35000").perform()
             priceMax.send_keys(u"55000")
         except Exception:
             print(sys.exc_info()[1])
+            self.fail("There are no element")
         #Производитель*******
         try:
             element = self.driver.find_element_by_xpath(
@@ -64,12 +63,12 @@ class TestBuySitilink(unittest.TestCase):
             Acer = self.driver.find_element_by_xpath(
                 "//*[@id='search-prepack']/div/div/div[2]/div/div[1]/div[3]/fieldset/ul/li[1]/div/a/label").click()
             Apple = self.driver.find_element_by_xpath(
-                "//*[@id='search-prepack']/div/div/div[2]/div/div[1]/div[3]/fieldset/ul/li[1]/div/a/label").click()
+                "//*[@id='search-prepack']/div/div/div[2]/div/div[1]/div[3]/fieldset/ul/li[3]/div/a/label").click()
             Asus = self.driver.find_element_by_xpath(
                 "//*[@id='search-prepack']/div/div/div[2]/div/div[1]/div[3]/fieldset/ul/li[4]/div/a/label").click()
+        except NoSuchElementException:
+            self.fail("There are no element")
 
-        except Exception:
-            print(sys.exc_info()[1])
 
 #        actions.move_to_element(Acer).click().perform()
  #       actions.move_to_element(Apple).click().perform()
@@ -77,7 +76,6 @@ class TestBuySitilink(unittest.TestCase):
 #        self.driver.execute_script("arguments[0].scrollIntoView();", priceMax)
 #        actions.move_to_element(priceMax).send_keys(u"35000").perform()
 
-        time.sleep(5)
 
     def tearDown(self):
         self.driver.get_screenshot_as_file("ya123.png")  # Сделаем скриншот страницы
--- test_task/test_runner.py
+++ test_task/test_runner.py
@@ -1,15 +1,15 @@
 import unittest
-import PriceSitilink, ikor
+import PriceSitilink, stepTest
 
 testLoad = unittest.TestLoader()
 tc1 = testLoad.loadTestsFromModule(PriceSitilink)
-#tc2 = testLoad.loadTestsFromModule(ikor)
+tc2 = testLoad.loadTestsFromModule(stepTest)
 
-#tc = unittest.TestSuite([tc1, tc2])
+tc = unittest.TestSuite([tc1, tc2])
 #runner = unittest.TextTestRunner(verbosity=2)
 #runner.run(suites)
 runner = unittest.TextTestRunner(verbosity=2)
-testResult = runner.run(tc1)
+testResult = runner.run(tc)
 print("errors")
 print(len(testResult.errors))
 print("failures")
